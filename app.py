949c3cf5f315715fbae5739bd1106015c178c9eb
celso <celsofranca@dcc.ufmg.br>
2019-12-23 12:15:36 -0300

    feat: add pre trained embedding layer.

--- app.py
+++ app.py
@@ -1,103 +1,98 @@
 import json
 
 import numpy as np
-import pandas as pd
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 
 from rnn_text_classifier import RNNTextClassifier
 
 
-def read_and_preprocess(dataset_name):
-    """ read and preprocess some dataset
-    :return: the dataset preprocessed train, test and validation splits
-    """
-
-    # read data
-    df = pd.read_csv(dataset_name)
-    df['consumer_complaint_narrative'] = df['consumer_complaint_narrative'].astype(str)
-
-    # data information
-    print("Data:\n", df.head())
-
-    print("Text classes = ", set(df["product"].values))
-
-    tokenizer = get_simple_tokenizer()
-
-    tokenizer.fit_on_texts(df['consumer_complaint_narrative'].values)
-
-    x = tokenizer.texts_to_sequences(df['consumer_complaint_narrative'].values)
-    x = tf.keras.preprocessing.sequence.pad_sequences(x, maxlen=configs["sequence_size"])
-
-    y = pd.get_dummies(df['product']).values
-
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.10, random_state=42)
-    x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.10, random_state=42)
-
-    print("Train shape:\n", x_train.shape, y_train.shape)
-    print("Test shape:\n", x_test.shape, y_test.shape)
-    print("Validation shape:\n", x_val.shape, y_val.shape)
-
-    return x_train, y_train, x_test, y_test, x_val, y_val
-
-
-def train(configs, x_train, y_train, x_val, y_val):
-    # rnn text classifier model
-    model = RNNTextClassifier(configs)
-
-    # training the model
-    model.fit(x_train, y_train, x_val, y_val)
-
-
-def test(configs, x_test, y_test):
-    model = tf.keras.models.load_model(configs["checkpoint_path"])
-    loss, acc = model.evaluate(x_test, y_test)
-    print('Loss: {:0.3f} - Accuracy: {:0.3f}'.format(loss, acc))
-
-
-def predict(new_text, configs):
-    tokenizer = get_simple_tokenizer()
-    tokenized_text = tokenizer.texts_to_sequences(new_text)
-    padded_text = tf.keras.preprocessing.sequence.pad_sequences(
-        tokenized_text,
-        maxlen=configs["sequence_size"]
+def get_splits(texts, scores):
+    # example splits
+    train_texts, test_texts, train_scores, test_scores = train_test_split(
+        texts, scores, test_size=0.10, random_state=42
     )
-    model = model = tf.keras.models.load_model(configs["checkpoint_path"])
-    predictions = model.predict(padded_text)
+    train_texts, val_texts, train_scores, val_scores = train_test_split(
+        train_texts, train_scores, test_size=0.10, random_state=42
+    )
 
-    labels = ['Mortgage', 'Credit reporting', 'Money transfers', 'Credit card',
-              'Prepaid card', 'Payday loan', 'Debt collection', 'Consumer Loan',
-              'Bank account or service', 'Other financial service', 'Student loan']
-    print(predictions, labels[np.argmax(predictions)])
+    return train_texts, train_scores, test_texts, test_scores, val_texts, val_scores
 
 
-def get_simple_tokenizer():
-    return tf.keras.preprocessing.text.Tokenizer(
-        num_words=configs["vocabulary_size"],
-        filters='!"#$%&()*+,-./:;<=>?@[\]^_`{|}~',
-        lower=True)
+def preprocessing_dataset(texts):
+    tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=configs["vocabulary_size"])
+    tokenizer.fit_on_texts(texts)
+    return tokenizer
+
+
+def get_word_embeddings(embeddings_path):
+    word_embeddings = {}
+    with open(embeddings_path, "r") as embeddings_file:
+        for line in embeddings_file:
+            word, embedding = line.split(maxsplit=1)
+            embedding = np.fromstring(embedding, 'f', sep=' ')
+            word_embeddings[word] = embedding
+    return word_embeddings
+
+
+def get_embedding_matrix(word_index, embeddings_path):
+    embeddings = get_word_embeddings(embeddings_path)
+    embedding_matrix = np.zeros((configs["vocabulary_size"], configs["embedding_size"]))
+    for word, i in word_index.items():
+        embedding_vector = embeddings.get(word)
+        if embedding_vector is not None and i < configs["vocabulary_size"]:
+            embedding_matrix[i] = embedding_vector
+    return embedding_matrix
+
+
+def get_texts_and_scores(dataset_path):
+    with open(dataset_path + "texts.txt") as texts_file:
+        texts = texts_file.readlines()
+    with open(dataset_path + "scores.txt") as scores_file:
+        scores = scores_file.readlines()
+
+    return texts, scores
 
 
 if __name__ == '__main__':
+
     # get the sample configs file
     with open("sample_configs.json") as config_file:
         configs = json.load(config_file)
 
+    # getting list of texts and scores
+    print("Reading dataset.")
+    texts, scores = get_texts_and_scores(dataset_path="data/datasets/acm/")
+
+    # creating vocabulary
+    print(f"Creating vocabulary with the {configs['vocabulary_size']} most frequent words.")
+    tokenizer = preprocessing_dataset(texts)
+
+    # prepare embedding matrix
+    print("Getting word embeddings.")
+    embedding_matrix = get_embedding_matrix(
+        tokenizer.word_index,
+        "data/embeddings/glove.6B.100d.txt")
+
     # train and test split
-    x_train, y_train, x_test, y_test, x_val, y_val = read_and_preprocess(
-        dataset_name="data/consumer_complaints_small.csv"
+    print("Getting train, test and validation splits.")
+    train_texts, train_scores, test_texts, test_scores, val_texts, val_scores, = get_splits(
+        texts,
+        scores
     )
 
-    # train model
-    train(configs, x_train, y_train, x_val, y_val)
+    # rnn text classifier model
+    model = RNNTextClassifier(configs, embedding_matrix)
 
-    # testing accuracy
-    test(configs, x_test, y_test)
+    # training the model
+    print("Trainng model.")
+    model.fit(train_texts, train_scores, val_texts, val_scores, tokenizer)
 
     # predict
-    new_text = [
-        "I am a victim of identity theft and someone stole my identity and personal information to open up a Visa "
-        "credit card account with Bank of America. The following Bank of America Visa credit card account do not "
-        "belong to me : XXXX."]
+    print("Predictin with sample text: modeling and analyzing java software architectures")
+    new_text = ["modeling and analyzing java software architectures."]
 
-    predict(new_text, configs)
+    predictions = model.predict(new_text, tokenizer)
+
+    labels = list(set(scores))
+    print(predictions, labels[np.argmax(predictions)])
--- rnn_text_classifier.py
+++ rnn_text_classifier.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import tensorflow as tf
 from sklearn.base import BaseEstimator, ClassifierMixin
 
@@ -6,16 +7,24 @@ class RNNTextClassifier(tf.keras.Model, BaseEstimator, ClassifierMixin):
     """ Defines a Long Short Term Memory (LSTM) based model as an approach in the text classification problem.
     """
 
-    def __init__(self, configs):
+    def __init__(self, configs, embedding_matrix):
         super(RNNTextClassifier, self).__init__()
 
         self.configs = configs
 
         # model definition
         self.model = tf.keras.Sequential([
-            tf.keras.layers.Embedding(self.configs["vocabulary_size"], self.configs["embedding_size"]),
+            # embedding layer
+            tf.keras.layers.Embedding(self.configs["vocabulary_size"],
+                                      self.configs["embedding_size"],
+                                      embeddings_initializer=tf.keras.initializers.Constant(embedding_matrix),
+                                      trainable=False
+                                      ),
+            # dropout layer
             tf.keras.layers.Dropout(0.2),
+            # lstm layer
             tf.keras.layers.LSTM(self.configs["hidden_size"], dropout=0.2, recurrent_dropout=0.2),
+            # dense layer
             tf.keras.layers.Dense(self.configs["number_of_classes"], activation='softmax'),
 
         ])
@@ -25,51 +34,61 @@ class RNNTextClassifier(tf.keras.Model, BaseEstimator, ClassifierMixin):
                            optimizer=tf.keras.optimizers.Adam(self.configs["learning_rate"]),
                            metrics=['accuracy'])
 
-    def fit(self, x_train, y_train, x_val, y_val):
-        """ Trains the RNNTextClassifier model for a maximum number of epochs.
-        :param x_train: input tensor with shape (m:samples, n: max text length).
-        :param y_train: target tensor with shape (m:samples, n: number of classes).
-        :param x_val: validation tensor with shape (v:samples, n: max text length).
-        :param y_val: target tensor with shape (v:samples, n: number of classes).
+    def fit(self, train_texts, train_scores, val_texts, val_scores, tokenizer):
+        """
+        Trains the RNNTextClassifier model for a maximum number of epochs.
+        :param train_texts: list of training texts.
+        :param train_scores: list of scores (classes) for each training text.
+        :param val_texts: list of validation texts,
+        :param val_scores: list of scores (classes) for each validation text.
+        :param tokenizer: text tokenization utility class.
         :return: the training history.
         """
+
+        x_train, y_train = self.preprocess(train_texts, train_scores, tokenizer)
+        x_val, y_val = self.preprocess(val_texts, val_scores, tokenizer)
+
         return self.model.fit(x=x_train,
                               y=y_train,
                               epochs=self.configs["epochs"],
                               batch_size=self.configs["batch_size"],
                               validation_data=(x_val, y_val),
-                              callbacks=self.get_callbacks()
+                              callbacks=[tf.keras.callbacks.EarlyStopping(
+                                  monitor='val_loss',
+                                  patience=3,
+                                  min_delta=0.0001)
+                              ]
                               )
 
-    def predict(self, x):
+    def predict(self, texts, tokenizer):
         """
-        :param x: input tensor with shape (m:samples, n: max text length).
-        :return: predicted tensor with shape (m:samples, n: number of classes).
+        :param texts: a list of texts.
+        :return: predicted tensor.
         """
 
-        padded_sequences = tf.keras.preprocessing.sequence.pad_sequences(
-            sequences=x,
+        tokenized_texts = tokenizer.texts_to_sequences(texts)
+
+        padded_texts = tf.keras.preprocessing.sequence.pad_sequences(
+            tokenized_texts,
             maxlen=self.configs["sequence_size"]
         )
 
-        return self.model.predict(padded_sequences)
+        return self.model.predict(padded_texts)
 
-    def get_callbacks(self):
+    def preprocess(self, texts, scores, tokenizer):
         """
-        :return: ModelCheckpoint and EarlyStopping callbacks.
+        Vectorize the text samples into a 2D integer tensor
+        :param texts: a list of texts.
+        :param scores: list of scores (classes) for each text.
+        :param tokenizer: text tokenization utility class
+        :return: 2D integer tensor.
         """
-        return [
-            tf.keras.callbacks.ModelCheckpoint(
-                filepath=self.configs["checkpoint_path"],
-                monitor='val_accuracy',
-                verbose=1,
-                save_best_only=True,
-                save_weights_only=False,
-                save_frequency=1
-            ),
-            tf.keras.callbacks.EarlyStopping(
-                monitor='val_loss',
-                patience=3,
-                min_delta=0.0001
-            )
-        ]
+        x = None
+        y = None
+
+        if not (texts is None or scores is None):
+            x = tokenizer.texts_to_sequences(texts)  # transforms each text in texts to a sequence of integers.
+            x = tf.keras.preprocessing.sequence.pad_sequences(x, maxlen=self.configs["sequence_size"])  # padding
+            y = pd.get_dummies(scores).values  # scores representation
+
+        return x, y
